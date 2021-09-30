8f1aeaacf5763b7ab663181d19356c3f04619bfa
Sohee <sohee3480@gmail.com>
2021-09-30 21:27:12 +0900

    확장함수 만들기

---
+++ app/src/main/java/com/example/musicplayer/MusicModelMapper.kt
@@ -0,0 +1,12 @@
+package com.example.musicplayer
+
+import com.example.musicplayer.service.MusicEntity
+
+fun MusicEntity.mapper(id: Long): MusicModel =
+    MusicModel(
+        id = id,
+        streamUrl = streamUrl,
+        coverUrl = coverUrl,
+        track = track,
+        artist = artist
+    )
