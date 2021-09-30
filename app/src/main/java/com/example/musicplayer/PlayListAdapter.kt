dc4df1067426213c0a03afbe3c31c2caf50169b9
Sohee <sohee3480@gmail.com>
2021-09-30 21:28:40 +0900

    리사이클러뷰 어댑터 만들기

---
+++ app/src/main/java/com/example/musicplayer/PlayListAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.musicplayer
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class PlayListAdapter(private val callback: (MusicModel) -> Unit) :
+    androidx.recyclerview.widget.ListAdapter<MusicModel, PlayListAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(item: MusicModel) {
+
+            val titleTxt = view.findViewById<TextView>(R.id.itemTrackTxt)
+            val artistTxt = view.findViewById<TextView>(R.id.itemArtistTxt)
+            val coverImg = view.findViewById<ImageView>(R.id.itemCoverImg)
+
+            titleTxt.text = item.track
+            artistTxt.text = item.artist
+
+            Glide.with(coverImg.context)
+                .load(item.coverUrl)
+                .into(coverImg)
+
+            if (item.isPlaying) {
+                itemView.setBackgroundColor(Color.GRAY)
+            } else {
+                itemView.setBackgroundColor(Color.TRANSPARENT)
+            }
+
+            itemView.setOnClickListener {
+                callback(item)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_music, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        currentList[position].also { musicModel ->
+            holder.bind(musicModel)
+        }
+    }
+
+    companion object{
+        val diffUtil = object : DiffUtil.ItemCallback<MusicModel>() {
+            override fun areItemsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
