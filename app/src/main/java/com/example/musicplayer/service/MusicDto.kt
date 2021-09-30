d283b61ad4b39da3f27faebc98daca13fb457a03
Sohee <sohee3480@gmail.com>
2021-09-30 21:26:50 +0900

    api 이용하기 + 데이터 클래스 만들기

---
+++ app/src/main/java/com/example/musicplayer/service/MusicDto.kt
@@ -0,0 +1,5 @@
+package com.example.musicplayer.service
+
+data class MusicDto(
+    val musics: List<MusicEntity>
+)
---
+++ app/src/main/java/com/example/musicplayer/service/MusicEntity.kt
@@ -0,0 +1,15 @@
+package com.example.musicplayer.service
+
+import com.google.gson.annotations.SerializedName
+
+data class MusicEntity(
+    @SerializedName("track")
+    val track: String,
+    @SerializedName("streamUrl")
+    val streamUrl: String,
+    @SerializedName("artist")
+    val artist: String,
+    @SerializedName("coverUrl")
+    val coverUrl: String
+) {
+}
