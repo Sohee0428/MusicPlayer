2cf1e75c186a39d474daed227a5440ce0b1cb772
Sohee <sohee3480@gmail.com>
2021-10-02 14:13:56 +0900

    모델 맵핑하기

--- app/src/main/java/com/example/musicplayer/MusicModelMapper.kt
+++ app/src/main/java/com/example/musicplayer/MusicModelMapper.kt
@@ -1,5 +1,6 @@
 package com.example.musicplayer
 
+import com.example.musicplayer.service.MusicDto
 import com.example.musicplayer.service.MusicEntity
 
 fun MusicEntity.mapper(id: Long): MusicModel =
@@ -9,4 +10,11 @@ fun MusicEntity.mapper(id: Long): MusicModel =
         coverUrl = coverUrl,
         track = track,
         artist = artist
+    )
+
+fun MusicDto.mapper() : PlayerModel =
+    PlayerModel(
+        playMusicList =  musics.mapIndexed { index, musicEntity ->
+                musicEntity.mapper(index.toLong())
+            }
     )
